19c70fdc3dd80dde948bf9aeeca3b58f761b8b47
Janderson Souza Matias <jandersonsouzamatias@Jandersons-MacBook-Pro.local>
2023-08-29 20:16:25 -0300

    create new dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,183 @@
-import { Center } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { embedDashboard } from "@superset-ui/embedded-sdk";
-import AuthService from "@/services/auth";
+import { Button, Center, HStack, Text, VStack } from '@chakra-ui/react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartData } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+import { useState } from 'react';
+import { DASHBOARD } from './mock';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const coachingSessionPerTeacher: ChartData<'doughnut', number[], string> = {
+  datasets: [
+    {
+      data: [4.3, 0.7],
+      backgroundColor: ['#3373CC', '#C7CBD1'],
+      borderColor: ['#ffffff', '#ffffff'],
+      borderWidth: 1,
+      circumference: 270,
+      rotation: 225,
+      weight: 30,
+    },
+  ],
+};
+export const teachersThatCompletedTheSecondCoachSession: ChartData<'doughnut', number[], string> = {
+  datasets: [
+    {
+      data: [48, 52],
+      backgroundColor: ['#3373CC', '#66CCCC'],
+      borderColor: ['#ffffff', '#66CCCC'],
+      borderWidth: 1,
+      rotation: 180,
+      weight: 30,
+    },
+  ],
+};
+
+export const teachingPracticesCount: ChartData<'doughnut', number[], string> = {
+  labels: DASHBOARD.teachingPractices.map((item) => item.name),
+  datasets: [
+    {
+      data: DASHBOARD.teachingPractices.map((item) => item.data.teachers),
+      backgroundColor: ['#D1F0F0', '#C2E0FF', '#3373CC', '#264673', '#66CCCC', '#297A7A'],
+      borderColor: ['#D1F0F0', '#C2E0FF', '#3373CC', '#264673', '#66CCCC', '#297A7A'],
+      borderWidth: 1,
+      rotation: 180,
+      weight: 30,
+    },
+  ],
+};
 
 const DashboardPage: React.FC = () => {
-  useEffect(() => {
-    embedDashboard({
-      id: "e2379a55-6999-4150-8cc0-933c13b584ff",
-      supersetDomain: import.meta.env.VITE_SUPERSET_DOMAIN,
-      mountPoint: document.getElementById("superset-container") as HTMLElement,
-      fetchGuestToken: async () => {
-        const token = (await AuthService.getGuestToken()).data;
-        console.log({ token });
-        return token;
-      },
-    });
-  }, []);
+  const [selected, setSelected] = useState(DASHBOARD.teachingPractices[0]);
+
+  const renderValue = (value: number, title: string) => (
+    <VStack flex={1} alignItems="center" p="16px" bg="#F2F4F7" borderRadius="16px" gap="0px">
+      <Text mb="4px" fontSize="40px" fontWeight={600}>
+        {value}
+      </Text>
+      <Text mt={0} textAlign="center" color="#111417" fontSize="16px">
+        {title}
+      </Text>
+    </VStack>
+  );
 
   return (
-    <Center flex={1}>
-      <div
-        id={"superset-container"}
-        style={{
-          width: "calc(100vw - 220px)",
-          height: "100vh",
-          backgroundColor: "#F5F5F5",
-        }}
-      />
-    </Center>
+    <VStack minH="100vh" w="100%" alignItems="flex-start" p="56px">
+      <HStack w="100%" gap="16px" mb="56px" alignItems="stretch">
+        <VStack flex={1} gap="16px" alignItems="stretch">
+          <Text color="#111417" fontSize="24px" fontWeight={600}>
+            Engagement
+          </Text>
+          <Text color="#111417" fontSize="16px" lineHeight="24px">
+            The numbers that represent coach engagement using the Coach platform in the selected period.
+          </Text>
+          <HStack gap="16px" alignItems="stretch">
+            {renderValue(DASHBOARD.engagement.teachersCoached, 'Teachers coached')}
+            {renderValue(DASHBOARD.engagement.activeCoaches, 'Active coaches')}
+            {renderValue(DASHBOARD.engagement.coachingSessions, 'Coaching sessions')}
+          </HStack>
+          <HStack gap="16px" alignItems="stretch">
+            <VStack flex={1} alignItems="center" p="16px" bg="#F2F4F7" borderRadius="16px">
+              <Center position="relative">
+                <Doughnut data={coachingSessionPerTeacher} />
+                <VStack
+                  top="55px"
+                  position="absolute"
+                  bg="#F2F4F7"
+                  borderRadius="50%"
+                  w="calc(100% - 60px)"
+                  h="calc(100% - 60px)"
+                  justifyContent="center"
+                >
+                  <Text fontSize="42px" fontWeight={600} color="#111417">
+                    {DASHBOARD.engagement.coachingSessionPerTeacher}
+                  </Text>
+                  <Text fontSize={16} color="#576375">
+                    Goal: {DASHBOARD.engagement.coachingSessionPerTeacherGoal}
+                  </Text>
+                </VStack>
+              </Center>
+              <Text mt="16px" textAlign="center" color="#111417">
+                Coaching sessions per teacher over last three months
+              </Text>
+            </VStack>
+            <VStack flex={1} alignItems="stretch" p="16px" bg="#F2F4F7" borderRadius="16px">
+              <Center position="relative">
+                <Doughnut data={teachersThatCompletedTheSecondCoachSession} />
+                <VStack
+                  top="35px"
+                  position="absolute"
+                  bg="#F2F4F7"
+                  borderRadius="50%"
+                  w="calc(100% - 60px)"
+                  h="calc(100% - 60px)"
+                  justifyContent="center"
+                >
+                  <Text fontSize="42px" fontWeight={600} color="#111417">
+                    {(
+                      (DASHBOARD.engagement.teacherThatCompletedSecondSession / DASHBOARD.engagement.teachersCoached) *
+                      100
+                    ).toFixed(0)}
+                    %
+                  </Text>
+                  <Text fontSize={16} color="#576375">
+                    Of teachers
+                  </Text>
+                </VStack>
+              </Center>
+              <Text mt="16px" textAlign="center" color="#111417">
+                Completed a second coach session
+              </Text>
+            </VStack>
+          </HStack>
+        </VStack>
+
+        <VStack flex={1} alignItems="stretch">
+          <Text color="#111417" fontSize="24px" fontWeight={600}>
+            Targeted improvement areas
+          </Text>
+          <Text color="#111417" fontSize="16px" lineHeight="24px">
+            Teaching practices teachers and coaches agreed to work on between coaching sessions
+          </Text>
+          <Center flex={1} alignItems="center" p="16px" bg="#F2F4F7" borderRadius="16px">
+            <Doughnut data={teachingPracticesCount} />
+          </Center>
+        </VStack>
+      </HStack>
+
+      <Text mb="8px" color="#111417" fontWeight={500} fontSize={'16px'}>
+        Select Teaching Practices to show
+      </Text>
+
+      <HStack mb="56px">
+        {DASHBOARD.teachingPractices.map((item) => (
+          <Button
+            py="8px"
+            px="12px"
+            fontWeight={400}
+            fontSize={'16px'}
+            borderRadius="full"
+            border="1px solid #DCE0E5"
+            onClick={() => setSelected(item)}
+            bg={selected === item ? '#C2E0FF' : '#ffffff'}
+            color={selected === item ? '#264673' : '#111417'}
+          >
+            {item.name}
+          </Button>
+        ))}
+      </HStack>
+
+      <Text color="#111417" fontSize="24px" fontWeight={600}>
+        {selected.name}
+      </Text>
+      <HStack>
+        graph
+        <VStack>
+          <HStack>a b c</HStack>
+          graph
+        </VStack>
+      </HStack>
+    </VStack>
   );
 };
 
---
+++ src/pages/Dashboard/mock.ts
@@ -0,0 +1,58 @@
+export const DASHBOARD = {
+  teachingPractices: [
+    {
+      name: 'Time on learning',
+      data: {
+        stars: { needsWork: 20, keepWorking: 30, needsAttention: 45, almostThere: 25, doingGreat: 35 },
+        teachers: 40,
+        teachersShowingImprovement: 25,
+        teacherOverdue: 35,
+      },
+    },
+    {
+      name: 'Supportive learning enviroment',
+      data: {
+        stars: { needsWork: 20, keepWorking: 30, needsAttention: 45, almostThere: 25, doingGreat: 35 },
+        teachers: 40,
+        teachersShowingImprovement: 25,
+        teacherOverdue: 35,
+      },
+    },
+    {
+      name: 'Positive behavioral expectations',
+      data: {
+        stars: { needsWork: 20, keepWorking: 30, needsAttention: 45, almostThere: 25, doingGreat: 35 },
+        teachers: 40,
+        teachersShowingImprovement: 25,
+        teacherOverdue: 35,
+      },
+    },
+    {
+      name: 'Effective teaching',
+      data: {
+        stars: { needsWork: 20, keepWorking: 30, needsAttention: 45, almostThere: 25, doingGreat: 35 },
+        teachers: 40,
+        teachersShowingImprovement: 25,
+        teacherOverdue: 35,
+      },
+    },
+    {
+      name: 'Critical thinking',
+      data: {
+        stars: { needsWork: 20, keepWorking: 30, needsAttention: 45, almostThere: 25, doingGreat: 35 },
+        teachers: 40,
+        teachersShowingImprovement: 25,
+        teacherOverdue: 35,
+      },
+    },
+  ],
+  engagement: {
+    teachersCoached: 145,
+    activeCoaches: 92,
+    coachingSessions: 332,
+    coachingSessionPerTeacher: 4.3,
+    coachingSessionPerTeacherGoal: 5,
+    teacherThatCompletedSecondSession: 70,
+  },
+  targetedImprovementAreas: {},
+};
