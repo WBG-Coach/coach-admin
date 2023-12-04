6043a4680f476eb2be6b3a436452f3d905ac3260
Janderson Souza Matias <jandersonsouzamatias@Jandersons-MacBook-Pro.local>
2023-12-04 14:49:32 -0300

    adjust logos

--- src/assets/images/logos/index.ts
+++ src/assets/images/logos/index.ts
@@ -1,4 +1,4 @@
 import CoachLogoSL from './CoachLogo.png';
-import CoachLogoNP from './CoachLogoNP.png';
+import CoachLogoNP from './CoachLogoNP.svg';
 
 export const CoachLogo = import.meta.env.VITE_COUNTRY === 'sl' ? CoachLogoSL : CoachLogoNP;
